9815eafc04b5e216cd6a178b0c0b24745adbd858
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-06-11 16:36:16 +0200

    Make it work (:

--- math.py
+++ math.py
@@ -1,28 +1,98 @@
+from pynput import mouse, keyboard
+import win32gui
+
+wx = 0
+wy = 0
+def callback(hwnd, extra):
+    if(win32gui.GetWindowText(hwnd) == 'Graphwar'):
+        global wx, wy
+
+        rect = win32gui.GetWindowRect(hwnd)
+        wx = rect[0]
+        wy = rect[1]
+        w = rect[2] - wx
+        h = rect[3] - wy
+        print("Window %s:" % win32gui.GetWindowText(hwnd))
+        print("\tLocation: (%d, %d)" % (wx, wy))
+        print("\t    Size: (%d, %d)" % (w, h))
+
+def main():
+    win32gui.EnumWindows(callback, None)
+
+if __name__ == '__main__':
+    main()
+
 currentDir = 1  
 
 def newDir(loc, dir):
+    
     global currentDir
-    l = f'(x-{loc}+abs(x-{loc}))({dir-currentDir})'
+    l = f'(x{-loc:+.2f}+abs(x{-loc:+.2f}))*({(dir-currentDir)/2:.2f})'
     '(x-1+abs(x-1))'
     currentDir = dir
     return l
     
-pixelToUnit = 25/387
+pixelToUnit = 50/851
 
 prevX = 0
-PrevY = 0
+prevY = 0
 
 def findSteepness(prevX, prevY, x, y):
-    return (y-prevY)/(x-prevX)
+    return (prevY - y)/(x-prevX)
 
 
+
+formel = ''
+
+
+def XtoUnit(x):
+    return (x - wx - 25) * pixelToUnit - 25
+
+clickCount = 0
 def onClick(x, y):
-    global prevX, prevY
-    steepness = findSteepness(prevX, prevY, x, y)
+    
+    global prevX, prevY, formel, clickCount, currentDir
+    print(f'X to unit: {XtoUnit(x):.3f}, normal x {x}')
+
+    if(clickCount > 0):
+        steepness = findSteepness(prevX, prevY, x, y)
+        print(f'Steepness: {steepness:.2f}')
+        
+        if(clickCount > 1):
+            formel += "+" + newDir(XtoUnit(prevX), steepness)
+        else:
+            formel += f'{steepness:.2f}x'
+            currentDir = steepness
+    
     prevX = x
     prevY = y
-    formel += "+" + newDir(x*pixelToUnit, steepness)
+    clickCount += 1
     
+def on_click(x, y, button, pressed):
+    
+    if pressed:
+        onClick(x, y)
+
+
+    
+import pyperclip
+def on_press(key):
+    global formel, clickCount
+    if key == keyboard.Key.enter:
+        print(formel)
+        pyperclip.copy(formel)
+        formel = ""
+        clickCount = 0
+
+mouse_listener = mouse.Listener(on_click=on_click)
+keyboard_listener = keyboard.Listener(on_press=on_press)
+
+mouse_listener.start()
+keyboard_listener.start()
+
+    
+mouse_listener.join()
+keyboard_listener.join()
 # formel = f'(x+abs(x))({currentDir})'
 
 # formel += "+" + newDir(1, 0)
